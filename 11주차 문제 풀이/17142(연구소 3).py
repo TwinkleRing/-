50c44b6d6b360c62b41920969017b74f9c74df74
YoungHun Kim <dlrmwl15@gmail.com>
2021-04-11 16:06:53 +0900

    Update 17142(연구소 3).py

--- 11주차 문제 풀이/17142(연구소 3).py
+++ 11주차 문제 풀이/17142(연구소 3).py
@@ -8,75 +8,73 @@ input = sys.stdin.readline
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-n, m = map(int, input().split()) # 연구소의 크기, 놓을 수 있는 바이러스 개수
-
-maps = []
-virus = [] 
-
-for x in range(n) :
-    tmp = list(map(int,input().split()))
-    for y in range(n) :
-        if tmp[y] == 2 : # 비활성 바이러스가 있다! 
-            virus.append((x, y, 0))
+def check(arr, visited) :
+    for i in range(n) :
+        for j in range(n) :
+            if arr[i][j] != 1 and visited[i][j] == 0 :
+                return False
     
-    maps.append(tmp)
+    return True # 다 퍼트렸다!
 
-
-def check(maps) :
-    for x in range(n) :
-        for y in range(n) :
-            if maps[x][y] == 0 : # 빈칸이 하나라도 있다!
-                return -1
-
-    return 0
-
-
-def bfs(start, maps) :
-    visited = [[0 for _ in range(n)] for _ in range(n)]
-    maps = deepcopy(maps)
-    q = deque()
-
-    q.extend(start)
+def bfs(start) :
+    global answer
+    array = deepcopy(maps)
+    
+    visited = [[0] * n for _ in range(n)]
     last_change = 0
+    
+    q = deque()
+    q.extend(start)
+
 
     # 0이 2로 바뀌는 마지막 순간만을 확인하면 된다.
     # 비활성 바이러스가 활성화 되는 경우는 '빈 칸에 바이러스를 퍼뜨리는 경우가 아니기 때문에
     while q :
         x, y, cnt = q.popleft()
         visited[x][y] = 1
-        for i in range(4) :
-            nx = x + dx[i]
-            ny = y + dy[i]
+
+        for k in range(4) :
+            nx = x + dx[k]
+            ny = y + dy[k]
             if 0 <= nx < n and 0 <= ny < n :
-                if not visited[nx][ny] and maps[nx][ny] != 1 : # 벽이 아니고, 방문 한 곳도 아니면 퍼트린다.
+                
+                if visited[nx][ny] == 0 and array[nx][ny] != 1 :
                     visited[nx][ny] = 1
 
-                    if maps[nx][ny] == 0 : # 빈칸이면 바이러스 퍼트린다.
-                        maps[nx][ny] = 2
+                    if array[nx][ny] == 0 :
+                        array[nx][ny] = 2
                         last_change = cnt + 1
 
                     q.append((nx, ny, cnt + 1))
-    
-    # 빈 칸이 있는지 확인.
-    val = check(maps)
-    if val == 0 :
-        return last_change
-    else :
-        return -1
 
 
-mins = 1e9
+    if check(array, visited) :
+        answer = min(answer, last_change)        
+
+
+#_____________________________________________________________
+
+n, m = map(int,input().split())
+
+maps = []
+virus = []
+
+for i in range(n) :
+    tmp = list(map(int,input().split()))
+    for j in range(n) :
+        if tmp[j] == 2 :
+            virus.append((i, j, 0))
+
+    maps.append(tmp)
 
 # 활성화 시킬 바이러스를 선택하는 경우의 수
 candidates = list(cb(virus, m))
+answer = 1e9
 
-for v in candidates :
-    result = bfs(v, maps)
-    if mins > result and result != -1 :
-        mins = result
+for candidate in candidates :
+    bfs(candidate)
 
-
-if mins == 1e9 :
+if answer == 1e9 :
     print(-1)
 else :
-    print(mins)
+    print(answer)
